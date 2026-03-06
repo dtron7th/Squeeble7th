67dccb217211e8d9110b1fee083cc95fbd53754a
dtron7th <dtron7th@gmail.com>
2026-03-06 19:09:16 +0000

    Mar 6, 2026, 2:09 PM

--- mobile-browser/android/app/src/main/java/com/vistadnet/MainActivity.java
+++ mobile-browser/android/app/src/main/java/com/vistadnet/MainActivity.java
@@ -2,7 +2,6 @@ package com.vistadnet;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
@@ -10,9 +9,6 @@ import android.webkit.WebSettings;
 public class MainActivity extends Activity {
 
     private WebView webView;
-    private Handler refreshHandler;
-    private Runnable refreshRunnable;
-    private static final int REFRESH_INTERVAL = 30000; // 30 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +31,10 @@ public class MainActivity extends Activity {
         
         // Load your website
         webView.loadUrl("http://vista-d-net.world/");
-        
-        // Initialize auto-refresh
-        initializeAutoRefresh();
     }
     
     @Override
     public void onBackPressed() {
         // Keep user in app
-    }
-    
-    private void initializeAutoRefresh() {
-        refreshHandler = new Handler();
-        refreshRunnable = new Runnable() {
-            @Override
-            public void run() {
-                if (webView != null) {
-                    webView.reload();
-                }
-                refreshHandler.postDelayed(this, REFRESH_INTERVAL);
-            }
-        };
-        
-        // Start auto-refresh after initial load
-        refreshHandler.postDelayed(refreshRunnable, REFRESH_INTERVAL);
-    }
-    
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Stop auto-refresh when app is destroyed
-        if (refreshHandler != null && refreshRunnable != null) {
-            refreshHandler.removeCallbacks(refreshRunnable);
-        }
     }
 }
